497d5b2c504d1b9cb2149c6ee033633261493354
hdhms5572 <hdhms5572@gmail.com>
2025-07-03 16:18:36 +0530

    Update 2_Datasets.py

--- App/pages/2_Datasets.py
+++ App/pages/2_Datasets.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from utils import call_powerbi_api, get_filtered_dataframes
+from utils import get_filtered_dataframes, apply_sidebar_style, show_workspace
+apply_sidebar_style()
+show_workspace()
 
 
 st.markdown("<h1 style='text-align: center;'>📊 Datasets</h1>", unsafe_allow_html=True)
@@ -58,4 +60,47 @@ with col2:
     st.pyplot(fig)
 
 
-st.dataframe(datasets_df)
+if "veiw_datasets" not in st.session_state:
+    st.session_state.veiw_datasets = False
+if "Explore_datasets_dataframe" not in st.session_state:
+    st.session_state.Explore_datasets_dataframe = False
+
+with st.container():
+    col1, col2, col3, col4, col5 = st.columns([1,3,3,4,1])
+    with col2:
+        if st.button("📄 View Datasets"):
+            st.session_state.veiw_datasets = True
+            st.session_state.Explore_datasets_dataframe = False
+    with col4:
+        if st.button("📄 Explore Datasets DataFrame"):
+            st.session_state.veiw_datasets = False
+            st.session_state.Explore_datasets_dataframe = True
+
+
+if st.session_state.veiw_datasets:
+
+    st.markdown(" 🔗 Datasets")
+    with st.container():
+        col1, col2, col3, col4, col5 = st.columns([3, 3, 3, 3, 2])
+        col1.markdown("<h5 style='margin-bottom: 0.5rem;'>🔖 ID</h5>", unsafe_allow_html=True)
+        col2.markdown("<h5 style='margin-bottom: 0.5rem;'>📛 Name</h5>", unsafe_allow_html=True)
+        col3.markdown("<h5 style='margin-bottom: 0.5rem;'>👤 ConfiguredBy</h5>", unsafe_allow_html=True)
+        col4.markdown("<h5 style='margin-bottom: 0.5rem;'>📅 CreatedDate</h5>", unsafe_allow_html=True)
+        col5.markdown("<h5 style='margin-bottom: 0.5rem;'>🔍 Explore Dataset</h5>", unsafe_allow_html=True)
+
+
+    for index, row in datasets_df.iterrows():
+        with st.container():
+            col1, col2, col3, col4, col5 = st.columns([3, 3, 4, 3, 2])
+            col1.markdown(f"**{row['id']}**")
+            col2.markdown(f"**{row['name']}**")
+            col3.markdown(f"`{row['configuredBy']}`")
+            col4.write(f"**{row['createdDate']}**")
+            col5.markdown(
+            f"""<a href="{row['webUrl']}" target="_blank"><button style='font-size: 0.9rem;'>🚀 Explore</button></a>""",
+            unsafe_allow_html=True
+            )
+
+elif st.session_state.Explore_datasets_dataframe:
+    st.dataframe(datasets_df)
+

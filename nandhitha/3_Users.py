89c82642b28b4c2bc50bd4e8da78565b2c8cc77a
hdhms5572 <hdhms5572@gmail.com>
2025-07-03 15:18:07 +0530

    Update 3_Users.py

--- nandhitha/3_Users.py
+++ nandhitha/3_Users.py
@@ -4,6 +4,9 @@ from utils import apply_sidebar_style
 apply_sidebar_style()
 from utils import show_workspace_header
 show_workspace_header()
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 st.title("👥 Users")
 
@@ -26,3 +29,56 @@ if users_df.empty:
 
 # Display user data
 st.dataframe(users_df[["displayName", "emailAddress", "groupUserAccessRight", "principalType"]])
+
+# Count users by group access right
+role_counts = users_df["groupUserAccessRight"].value_counts()
+labels = role_counts.index
+sizes = role_counts.values
+
+
+role_colors = {
+    "Admin": "OrangeRed",
+    "Contributor": "DodgerBlue",
+    "Viewer": "DimGray",
+    "Member": "MediumSeaGreen"
+}
+colors = [role_colors.get(role, "LightGray") for role in labels]
+
+col_left, col_center, col_right = st.columns([1, 2, 1])  
+
+with col_center:
+    fig, ax = plt.subplots(figsize=(3.5, 3.5))  
+    wedges, texts, autotexts = ax.pie(
+        sizes,
+        labels=labels,
+        autopct="%1.1f%%",
+        startangle=140,
+        colors=colors,
+        wedgeprops=dict(width=0.3),
+        textprops={'fontsize': 8, 'color': 'black'}
+    )
+    ax.set_title("Group User Access Rights", fontsize=10)
+    ax.axis("equal")
+    st.pyplot(fig)
+
+# Extract domain names
+users_df["Domain"] = users_df["emailAddress"].str.split("@").str[-1]
+domain_counts = users_df["Domain"].value_counts().sort_values(ascending=True)  
+
+col_left, col_chart, col_right = st.columns([1, 3, 1])
+
+with col_chart:
+    fig, ax = plt.subplots(figsize=(4.8, 2.8))  
+    sns.barplot(
+        x=domain_counts.values,   #  X-axis: Count
+        y=domain_counts.index,    # Y-axis: Email Domain
+        palette=["SkyBlue"] * len(domain_counts),
+        ax=ax
+    )
+    ax.set_title("Workspace Access by Email Domain", fontsize=12, weight='bold')
+    ax.set_xlabel("User Count", fontsize=9)
+    ax.set_ylabel("Email Domain", fontsize=9)
+    ax.tick_params(axis='x', labelsize=8)
+    ax.tick_params(axis='y', labelsize=8)
+    sns.despine()
+    st.pyplot(fig)

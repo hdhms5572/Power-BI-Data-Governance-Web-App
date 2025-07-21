efbe70306c24f458e869348bc7e41e1a3c91402c
hdhms5572 <hdhms5572@gmail.com>
2025-07-21 15:50:02 +0530

    Update 2_Datasets.py

--- App/pages/2_Datasets.py
+++ App/pages/2_Datasets.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from utils import  render_profile_header
 import plotly.express as px
-from utils import get_cached_workspace_data, apply_sidebar_style, show_workspace
+from utils import get_cached_workspace_data, apply_sidebar_style, show_workspace, add_logout_button
 
 def inject_external_style():
     with open("static/style.css") as f:
@@ -14,9 +14,11 @@ def inject_external_style():
 
 # Initial UI setup
 apply_sidebar_style()
+render_profile_header()
+add_logout_button()
 show_workspace()
 inject_external_style()
-render_profile_header()
+
 col1, col2, col3 = st.columns(3)
 with col2:
     st.image("./images/dover_log.png")

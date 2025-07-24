31aa50965d1d9f044572d5e3cde265c3e2ae14c6
hdhms5572 <hdhms5572@gmail.com>
2025-07-24 11:23:12 +0530

    Update 4_Activity_Analysis.py

--- App/pages/4_Activity_Analysis.py
+++ App/pages/4_Activity_Analysis.py
@@ -260,16 +260,21 @@ from utils import get_cached_workspace_data, apply_sidebar_style, show_workspace
 from utils import  render_profile_header, add_logout_button
 from utils import handle_activity_upload,apply_activity_status
 
+apply_sidebar_style()
 def inject_external_style():
     with open("static/style.css") as f:
         css = f.read()
         st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
-
-apply_sidebar_style()
-render_profile_header()
-add_logout_button()
-show_workspace()
 inject_external_style()
+
+if not (st.session_state.get("access_token") and
+        st.session_state.get("user_email")):
+    st.warning("🔐 Authentication Required")
+    st.stop()
+
+add_logout_button()
+render_profile_header()
+show_workspace()
 
 col1, col2, col3 = st.columns(3)
 with col2:
@@ -282,10 +287,6 @@ This dashboard provides a centralized view of all user interactions with reports
 Explore usage trends, identify top artifacts and users, analyze activity patterns, and detect unused resources to improve data governance.
 </div><hr>
 """, unsafe_allow_html=True)
-if not (st.session_state.get("access_token") and st.session_state.get("workspace_ids") and st.session_state.get("user_email")):
-    st.warning("❌ Missing access token or selected workspaces. Please authenticate.")
-    st.stop()
-
 
 token = st.session_state.access_token
 workspace_ids = st.session_state.workspace_ids
